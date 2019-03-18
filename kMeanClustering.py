7d4c2ebf44eedf247df67312a6a404131d94142f
Jason T Baumbach <jatlast@hotmail.com>
2019-03-18 17:50:54 -0400

    Successfully combined file parsings. Still working on loop for mean groupings and recalculations

--- kMeanClustering.py
+++ kMeanClustering.py
@@ -34,26 +34,39 @@ import re
 # allow command line options
 import argparse
 parser = argparse.ArgumentParser(description="perform the k-means clustering on 1 to 2-dimensional data")
-parser.add_argument("-f", "--filename", default="./data/HW_6_data_file.dat", help="file name (and path if not in . dir)")
+parser.add_argument("-f", "--filename", default="./data/HW_6_data_1D.dat", help="file name (and path if not in . dir)")
 parser.add_argument("-c", "--clusters", type=int, choices=[2, 3], default=2, help="number of clusters to try")
-#parser.add_argument("x", type=int, help="the base")
-#parser.add_argument("y", type=int, help="the exponent")
 parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2], help="increase output verbosity")
 args = parser.parse_args()
 
+# create a dictionary of list objects equal to the number of clusters to test
+clust_dict = {}
+for i in range(1, args.clusters + 1):
+    clust_dict[i] = []
+
 if args.verbosity == 2:
-    print(f"filename={args.filename} : clusters={args.clusters}")
+    print(f"filename={args.filename} : clusters={args.clusters} : len(clust_dict)={len(clust_dict)}")
 elif args.verbosity == 1:
-    print(f"{args.filename} : {args.clusters}")
-else:
-    print("v = 0")
+    print(f"{args.filename} : {args.clusters} : {len(clust_dict)}")
 
 # compute Euclidean distance between any two given vectors with any length.
 # Note: adapted from CSC 587 Adv Data Mining, HW02
-def EuclideanDistanceBetweenTwoPoints(One, Two):
+# Note: a return value < 0 = Error
+def EuclideanDistanceBetweenTwoVectors(vOne, vTwo):
     distance = 0
-    distance += math.pow((abs(One - Two)), 2)
+    v_one_len = len(vOne)
+    v_two_len = len(vTwo)
+    # vOne & vTwo must be of equal length
+    if(v_one_len != v_two_len):
+        return -1
+
+    for p in range(0, v_one_len):
+        distance += math.pow((abs(vOne[p] - vTwo[p])), 2)
     return math.sqrt(distance)
+
+def OneDimensionalDistanceBetweenTwoPoints(One, Two):
+    distance = 0
+    return abs(One - Two)
 
 # 1D scatterplots
 def Scatterplot1D(x):
@@ -78,74 +91,65 @@ def Scatterplot2D(x, y=0):
     plt.ylabel('y')
     plt.show()
 
-# create a dictionary of list objects equal to the number of clusters to test
-clust_dict = {}
-for i in range(1, args.clusters + 1):
-    clust_dict[i] = []
-
-print(f"clust_dict({len(clust_dict)})")
-
 # read in the 1-dimensional data file
 with open(args.filename, mode='r') as data_file:
-#    line_one = data_file.readline()
-    # dimensionality = 0
-    # d1 = re.compile(r'(\d+\.\d+)')
-    # d2 = re.compile(r'(\d+\.\d+)\s+(\d+\.\d+)')
-
-    # if d2.match(line_one):
-    #     dimensionality = 2
-    # elif d1.match(line_one):
-    #     dimensionality = 1
-    # else:
-    #     print("Error: unknown file dimensionality")
-    #     exit -1
-
-    # match = re.search(r'(\d+\.\d+)\s+(\d+\.\d+)?', line)
-    # if match:
-    #     print(match.group())
-    #     print(match.group(1))
-    #     print(match.group(2))
-
-    i = 0
     x = []
     y = []
     # parse data file
     for line in data_file:
-        match = re.search(r'(\d+\.\d+)\s+(\d+\.\d+)?', line)
+        match = re.search(r'(\d+\.\d+)\s*(\d+\.\d+)?', line)
         if match:
             if match.group(1):
                 x.append(float(match.group(1)))
             if match.group(2):
                 y.append(float(match.group(2)))
+        else:
+            print(f"Warning: no match for line ({line})")
+    x_len = len(x)
+    y_len = len(y)
+    mean_dict = {}
+    for j in range(1, args.clusters + 1):
+        rand_int = random.randint(1,x_len)
+        if y_len > 0:
+            mean_dict[j] = (x[rand_int], y[rand_int])
+        else:
+            mean_dict[j] = x[rand_int]
+    
+    for i in range(0, x_len):
+        min_distance = 10000
+        min_index = 0
+        for j in range(1, args.clusters + 1):
+            if y_len > 0:
+                vec_two = (x[i], y[i])
+                EuclideanDistance = EuclideanDistanceBetweenTwoVectors(mean_dict[j], vec_two)
+                if EuclideanDistance < min_distance:
+                    min_distance = EuclideanDistance
+                    min_index = j
+            else:
+                distance_1D = abs(mean_dict[j] - x[i])
+                if distance_1D < min_distance:
+                    min_distance = distance_1D
+                    min_index = j
+        if y_len > 0:
+            clust_dict[min_index].append(vec_two)
+        else:
+            clust_dict[min_index].append(x[i])
 
-    rand_int = random.randint(1,len(x))
-    if len(y) > 0:
-        Scatterplot2D(x, y)
-    else:
-        Scatterplot1D(x)
+    print(f"x_len({x_len}):y_len({y_len})")
+    print(f"mean_dict({mean_dict})")
+#    print(f"clust_dict({clust_dict})")
+    for i in range(1, args.clusters + 1):
+        print(f"clust len({len(clust_dict[i])}) mean({np.mean(clust_dict[i])})")
 
-#    Scatterplot1D(x)
-#    print(f"{x}")
+    # if y_len > 0:
+    #     Scatterplot2D(x, y)
+    # else:
+    #     Scatterplot1D(x)
+    # print(f"{rand_int} : {i} : {x_len}")
 
-    print(f"{rand_int} : {i} : {len(x)}")
+    # vec_one = (x[rand_int], y[rand_int])
+    # vec_two = (x[0], y[0])
 
-    EuclideanDistance = EuclideanDistanceBetweenTwoPoints(x[rand_int], x[1])
-    print(f"Euclidean distance = {EuclideanDistance}")
-
-# read in the 1-dimensional data file
-with open('./data/HW_6_data_2D.dat', mode='r') as data_2D:
-#    print(data_2D.readline())
-    i = 0
-    x = []
-    y = []
-    # parse data file
-    for line in data_2D:
-        x.append([])
-        y.append([])
-        x[i], y[i] = [float(n) for n in line.split('  ')]
-        i += 1
-
-    rand_int = random.randint(1,i)
-    print(f"{rand_int} : {i} : {len(x)}")
-#    Scatterplot2D(x, y)
+    # EuclideanDistance = EuclideanDistanceBetweenTwoVectors(vec_one, vec_two)
+    # print(f"Euclidean distance = {EuclideanDistance}")
 
